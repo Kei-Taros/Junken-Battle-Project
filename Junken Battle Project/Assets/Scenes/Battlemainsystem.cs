f1676a6ddbeb4c501a0705ec6c7f429dff3e3d3e
Kei-Taros <orz.www127@gmail.com>
2019-08-03 23:43:44 +0900

    master

---
+++ Junken Battle Project/Assets/Scenes/Battlemainsystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battlemainsystem : MonoBehaviour
+{
+
+    // JunkenHand
+    public int iHand,EnemyHand;
+
+    //JunkenCounter
+    int iGucount = 0,iChokicount =0,iPacount = 0;
+
+    //Player of Turn
+    bool iTurn,EnemyTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iHand = 0;
+        UnityEngine.Random.Range(1,4);
+
+    }
+
+    //GuBotton　process
+    public void GuOnClick()
+    {
+        iHand = 1;
+        iGucount += 1;
+    }
+
+    //GuBotton　process
+    public void ChokiOnClick()
+    {
+        iHand = 2; 
+        iChokicount += 1;
+    }
+
+    //GuBotton　process
+    public void PaOnClick()
+    {
+        iHand=3;
+        iPacount += 1;
+    }
+
+    //JunkenJuge
+    public void juge()
+    {
+        
+    }
+
+}
---
+++ Junken Battle Project/Assets/Scenes/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    // HitPoint
+    public int iHP, EnemyHP;
+
+    // Atacck
+    public int iAT, EnemyAT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iHP = 20;
+        EnemyHP = 20;
+        iAT = 1;
+        EnemyAT = 1;
+    }
+    public void onDamege(int _damege)
+    {
+        iHP -= _damege;
+    }
+}
