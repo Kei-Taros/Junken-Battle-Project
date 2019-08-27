d958c65b51036f8465ee253395bbc29c2bec4dc0
Kei-Taros <orz.www127@gmail.com>
2019-08-27 22:13:39 +0900

    no message

--- Junken Battle Project/Assets/Scenes/Battlemainsystem.cs
+++ Junken Battle Project/Assets/Scenes/Battlemainsystem.cs
@@ -8,10 +8,22 @@ public class Battlemainsystem : MonoBehaviour
     //自分と敵の変数定義
     public Unit Player;
     public Unit Enemy;
+
+    //GameObject
     public GameObject ResultPanel;
     public GameObject Enemy_gu;
     public GameObject Enemy_choki;
     public GameObject Enemy_pa;
+    public GameObject WIN;
+    public GameObject LOSE;
+    public GameObject DROW;
+    public GameObject LastWIN;
+    public GameObject LastLOSE;
+    public Button gu_button;
+    public Button choki_button;
+    public Button pa_button;
+
+
     int iHand;
     int EnemyHand;
 
@@ -22,38 +34,52 @@ public class Battlemainsystem : MonoBehaviour
     {
         IsPlayerturn = true;
         IsGameOver = false;
-        ResultPanel.SetActive(false);
-        Enemy_gu.SetActive(false);
-        Enemy_gu.SetActive(false);
-        Enemy_gu.SetActive(false);
+        FalseBrothers();
+        ButtonTrueBrothes();
+
     }
 
-    void ViewResult()
+    IEnumerator ViewResult()
     {
-        ResultPanel.SetActive(true);
-    }
 
+        yield return new WaitForSeconds(1f);
+        ResultPanel.SetActive(true);
+        
+        if (Enemy.HP <= 0)
+        {
+            LastWIN.SetActive(true);
+            ButtonfalseBrothes();
+        }
+        else if (Player.HP <= 0)
+        {
+            LastLOSE.SetActive(true);
+            ButtonfalseBrothes();
+        }
+    }
     float second = 0f;
     void Update()
     {
+        
         if (IsGameOver)
         {
-            ViewResult();
+            StartCoroutine(ViewResult());
         }
 
         if (!IsPlayerturn)
         {
             second += Time.deltaTime;
             if (second >= 1f)
-            {
+            { 
                 second = 0;
+                FalseBrothers();
                 //EnemyHand = Random.Range(0,3);
                 EnemyHand = 0;
                 EnemyHandy();
-                Juge();
+                StartCoroutine(Juge());
+                StartCoroutine(FalseSisters());
+                StartCoroutine(IsPlayerturnTrue());
                 IsPlayerturn = true;
             }
-            
         }
 
         if (Player.HP == 0 || Enemy.HP == 0)
@@ -62,24 +88,29 @@ public class Battlemainsystem : MonoBehaviour
         }
     }
 
+
     public void PushAttackButtonn(int number)
     {
         if (IsPlayerturn)
         {
+            
             switch (number)
             {
                 case 0:
                     iHand = 0;
+                    ButtonfalseSisters();
                     IsPlayerturn = false;
                     break;
 
                 case 1:
                     iHand = 1;
+                    ButtonfalseSisters();
                     IsPlayerturn = false;
                     break;
 
                 case 2:
                     iHand = 2;
+                    ButtonfalseSisters();
                     IsPlayerturn = false;
                     break;
 
@@ -89,41 +120,43 @@ public class Battlemainsystem : MonoBehaviour
             
         }
     }
-
-    private void Juge()
+    IEnumerator IsPlayerturnTrue()
     {
+        yield return new WaitForSeconds(2.0f);
+        ButtontrueSisters();
+    }
+
+    IEnumerator Juge()
+    {
+        yield return new WaitForSeconds(0.5f);
         if ( iHand==EnemyHand)
         {
-            Debug.Log("Drow");
+            DROW.SetActive(true);
         }
         else if (iHand==0 && EnemyHand == 1)
         {
             Enemy.OnDamege(Player.AT);
-            Debug.Log("WIN");
+            WIN.SetActive(true);
         }
         else if (iHand == 1 && EnemyHand == 2)
         {
             Enemy.OnDamege(Player.AT);
-            Debug.Log("WIN");
+            WIN.SetActive(true);
         }
-        else if (iHand == 2 && EnemyHand == 3)
+        else if (iHand == 2 && EnemyHand == 0)
         {
             Enemy.OnDamege(Player.AT);
-            Debug.Log("WIN");
+            WIN.SetActive(true);
         }
         else
         {
             Player.OnDamege(Enemy.AT);
-            Debug.Log("Lose");
+            LOSE.SetActive(true);
         }
     }
 
     private void EnemyHandy()
     {
-        Enemy_gu.SetActive(false);
-        Enemy_choki.SetActive(false);
-        Enemy_pa.SetActive(false);
-
         if (EnemyHand == 0)
         {
             Enemy_gu.SetActive(true);
@@ -137,4 +170,56 @@ public class Battlemainsystem : MonoBehaviour
             Enemy_pa.SetActive(true);
         }
     }
+
+    public void FalseBrothers()
+    {
+        Enemy_gu.SetActive(false);
+        Enemy_choki.SetActive(false);
+        WIN.SetActive(false);
+        LOSE.SetActive(false);
+        DROW.SetActive(false);
+        LastWIN.SetActive(false);
+        LastLOSE.SetActive(false);
+    }
+
+    IEnumerator FalseSisters()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Enemy_gu.SetActive(false);
+        Enemy_choki.SetActive(false);
+        Enemy_pa.SetActive(false);
+        WIN.SetActive(false);
+        LOSE.SetActive(false);
+        DROW.SetActive(false);
+    }
+
+    void ButtonTrueBrothes()
+    {
+        gu_button.GetComponent<Button>().interactable=true;
+        choki_button.GetComponent<Button>().interactable = true;
+        pa_button.GetComponent<Button>().interactable = true;
+
+    }
+    void ButtonfalseBrothes()
+    {
+        gu_button.GetComponent<Button>().interactable = false;
+        choki_button.GetComponent<Button>().interactable = false;
+        pa_button.GetComponent<Button>().interactable = false;
+        
+
+    }
+    void ButtontrueSisters()
+    {
+        gu_button.GetComponent<Button>().enabled = true;
+        choki_button.GetComponent<Button>().enabled = true;
+        pa_button.GetComponent<Button>().enabled = true;
+    }
+
+    void ButtonfalseSisters()
+    {
+        gu_button.GetComponent<Button>().enabled = false;
+        choki_button.GetComponent<Button>().enabled = false;
+        pa_button.GetComponent<Button>().enabled = false;
+    }
+
 }
